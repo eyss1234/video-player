a0cc8c99e620299c7ee6946a8dfaf9c427f03e55
Emily Santos <eyssantos@gmail.com>
2022-10-14 16:59:21 +0100

    add video pause and play on click functionality

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
-
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
 
 // Progress Bar ---------------------------------- //
@@ -19,3 +34,6 @@
 // Fullscreen ------------------------------- //
 
 
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
